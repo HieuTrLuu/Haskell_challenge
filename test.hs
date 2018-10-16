b4aed03e6597380c8b46faaa6d2693fa0d172d24
Hieu T Luu <thl1g15@soton.ac.uk>
2018-10-16 11:47:24 +0100

    second work sheet

--- test.hs
+++ test.hs
@@ -4,6 +4,11 @@ fetch n [] = error "Empty list"
 fetch 0 (x:_) = x
 fetch n (_:xs) = fetch (n-1) xs
 
+--exercise5
+split :: [a] -> ([a], [a])
+split myList = splitAt (((length myList) + 1) `div` 2) myList
+
+
 --using head and tail
 {--
 third :: [a] -> a
@@ -23,3 +28,38 @@ third (a:b:c:d) = c
 third :: [a] -> a
 third (x:xs) = xs !! 1
 --}
+
+--using pattern matching
+{--
+safetail :: [a] -> [a]
+safetail [] = []
+safetail (x:xs) = [last xs]
+--}
+
+listLength :: [a] -> Int
+listLength [] = 0
+listLength (x:xs) = 1 + listLength xs
+
+--using guard equation
+
+safetail :: [a] -> [a]
+safetail (x:xs) 
+    | listLength (x:xs) == 0 = []
+    | listLength (x:xs) >= 1 = [last xs]
+
+--exercise8
+
+(||) :: Bool -> Bool -> Bool
+True || True = True
+True || False = True
+False || True = True
+False || False = False
+
+--exercise9
+
+--enc :: Int -> String -> String
+
+
+--exercise10
+
+
