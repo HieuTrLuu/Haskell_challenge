df3d93e16629f13723d78b65322a64e39a7adf16
Hieu T Luu <thl1g15@soton.ac.uk>
2018-10-23 09:55:49 +0100

    comment on the ex1.hs

--- ex1.hs
+++ ex1.hs
@@ -1,11 +1,26 @@
 -- Exercise 1
 -- split a given list into sub-lists 
 -- each of these must be strictly ascending, descending, or equal
+
+--HELPER function
+
+positions :: Eq a => a -> [a] -> [Int]
+positions x xs = [ index | (y, index) <- zip xs [0..], x==y]
+
+-- Exercise 1
+-- split a given list into sub-lists 
+-- each of these must be strictly ascending, descending, or equal
+orderList :: Ord a => [(a,a)] -> [Ordering]
+orderList a = [ compare x y | (x,y) <- a  ]
+
+helperFunc :: [(Ordering,Ordering)] -> [Bool]
+helperFunc a = [ (x == y) | (x,y) <- a]
+
+
 splitSort :: Ord a => [a] -> [[a]] 
-splitSort ns = [[]]
+splitSort ns = splitPlaces (positions False (list2 list(ns))) ns
+    where list ns = positions False (helperFunc (orderList ns),
+          list2 ns = [ x+1 | x <- ns]
 
-
-helperFunc x:nx = compare x head(nx) helperFunc(tail nx)
-
--- map compare (x:xs) 
--- fold etc
+-- NEED to fix the syntax of where
+-- list ns = positions False (helperFunc (orderList ns) : find the position where the element is False
