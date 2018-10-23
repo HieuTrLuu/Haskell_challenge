496f0fab05bcbb24a06550c7ed75810ff841fcd5
hieu t luu <hieutluu@hieus-MacBook-Pro.local>
2018-10-23 10:52:33 +0100

    split but the equal part

--- ex1.hs
+++ ex1.hs
@@ -1,26 +1,29 @@
 -- Exercise 1
--- split a given list into sub-lists 
+-- split a given list into sub-lists
 -- each of these must be strictly ascending, descending, or equal
 
 --HELPER function
+import Data.List.Split
 
 positions :: Eq a => a -> [a] -> [Int]
 positions x xs = [ index | (y, index) <- zip xs [0..], x==y]
 
 -- Exercise 1
--- split a given list into sub-lists 
+-- split a given list into sub-lists
 -- each of these must be strictly ascending, descending, or equal
 orderList :: Ord a => [(a,a)] -> [Ordering]
 orderList a = [ compare x y | (x,y) <- a  ]
 
-helperFunc :: [(Ordering,Ordering)] -> [Bool]
-helperFunc a = [ (x == y) | (x,y) <- a]
+boolTransform :: [(Ordering,Ordering)] -> [Bool]
+boolTransform a = [ (x == y) | (x,y) <- a]
 
+splitSort :: Ord a => [a] -> [[a]]
+splitSort ns = splitPlaces truePlaces ns
+ where tupleNumber = zip ns (tail ns)
+       tupleOrd = zip (orderList tupleNumber) (tail (orderList tupleNumber))
+       oneDown = positions False (boolTransform tupleOrd)
+       truePlaces = [x+1 | x <- oneDown ]
 
-splitSort :: Ord a => [a] -> [[a]] 
-splitSort ns = splitPlaces (positions False (list2 list(ns))) ns
-    where list ns = positions False (helperFunc (orderList ns),
-          list2 ns = [ x+1 | x <- ns]
 
 -- NEED to fix the syntax of where
 -- list ns = positions False (helperFunc (orderList ns) : find the position where the element is False
