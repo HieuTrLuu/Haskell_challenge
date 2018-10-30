ca6c63968d40af301507d7d64e45cdd0759aca3b
Hieu T Luu <thl1g15@soton.ac.uk>
2018-10-30 14:52:10 +0000

    2nd attemp at ex2.hs

--- ex2.hs
+++ ex2.hs
@@ -1,5 +1,9 @@
-import Data.List (maximumBy)
+import Data.List
 import Data.Function (on)
+data Set a = Empty | Set a (Set a)
+  deriving (Read, Show)
+
+
 lcs :: Ord a => [a] -> [a] -> [a]
 lcs xs ys = snd $ lcs' xs ys
 
@@ -13,6 +17,15 @@ lcs' (x:xs) (y:ys)
 lcs' [] _ = (0, [])
 lcs' _ [] = (0, [])
 
+fromList :: [a] -> Set a
+fromList []     = Empty
+fromList (x:xs) = Set x (fromList xs)
+
+a = [1,3,5,4,2,5,4,6,1,2]
+test = fromList a --WTF tho ?
+
+
+
 {--
 What is r1 and r2, l1 and l2 in this case ?
 r1 and r2 with @ is the result of the function that have first index is l1 or l2
@@ -22,25 +35,27 @@ good example, remember this
 Hence, to do ex2 using list comprehension and recursion to go compare every elt while saving the largest sub set
 --}
 
-
-a = [1,3,5,4,2,5,4,6,1,2]
-
-b = [6,2,3,4]
-
-c = [1,2,3]
+-- test = [[1,2,3], [0,1,3], [1,3,4]]
+-- test2 = maximumBy (compare `on` length)  (helper test)
+--
+-- a = [1,3,5,4,2,5,4,6,1,2]
+--
+-- b = [6,2,3,4]
+--
+-- c = [1,2,3]
 
 
 -- Exercise 2
 -- longest common sub-list of a finite list of finite list
 
---helper function
-helper :: Ord a => [[a]] -> [[a]]
-helper xs = [lcs lists1 lists2 | lists1 <- xs, lists2 <- xs, lists1/=lists2]
+longestCommonSubList :: Eq a => [[a]] -> [a]
+longestCommonSubList [[]] = []
+-- longestCommonSubList xs =
+--  where
+--   list = sort $ concat xs
 
--- longestCommonSubList :: Eq a => [[a]] -> [a]
--- longestCommonSubList [[]] = []
--- longestCommonSubList xs =  maximumBy (compare `on` length)  (helper xs)
-
-test = [[1,2,3], [0,1,3], [1,3,4]]
-
-test2 = maximumBy (compare `on` length)  (helper test)
+getList :: Eq a => [[a]] -> [a]
+getList [[]] = []
+getList (x:xs) =
+ where
+  list = sort $ concat (x:xs)
