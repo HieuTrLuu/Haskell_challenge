968aa50ca7848378d560fb62593c8638a97bf99c
Hieu T Luu <thl1g15@soton.ac.uk>
2018-10-30 11:29:46 +0000

    debug ex14.hs

---
+++ ex14.hs
@@ -0,0 +1,9 @@
+-- Exercise 14
+-- extract both components from a square shell pair and apply the (curried) function
+unPairAndApply :: Int -> (Int -> Int -> a) -> a
+unPairAndApply n f = f 0 0
+
+unPair :: Int -> (Int -> Int)
+unPair z | z - (m^2) < m = (z - (m^2), m)
+         | otherwise = (m , ((m^2) +2*m -z))
+ where m = floor $ sqrt z
