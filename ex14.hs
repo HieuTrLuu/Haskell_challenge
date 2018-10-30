a3b8cd49146b1bc9bbc47d8c99795bb053c60d70
hieu t luu <hieutluu@hieus-MacBook-Pro.local>
2018-10-30 11:51:44 +0000

    need to ask lecturer abt this

--- ex14.hs
+++ ex14.hs
@@ -3,7 +3,7 @@
 unPairAndApply :: Int -> (Int -> Int -> a) -> a
 unPairAndApply n f = f 0 0
 
-unPair :: Int -> (Int -> Int)
-unPair z | z - (m^2) < m = (z - (m^2), m)
-         | otherwise = (m , ((m^2) +2*m -z))
- where m = floor $ sqrt z
+unPair :: Int -> (Int,Int)
+unPair z | (z - m^2) < m = (z - m^2, m)
+         | otherwise = (m , m^2 + 2*m -z)
+ where m = fromIntegral $ sqrt z
