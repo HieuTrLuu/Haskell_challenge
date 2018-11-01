5404f7dfadc18f57c435ee2f6990955e1ae1dbb2
hieu t luu <hieutluu@hieus-MacBook-Pro.local>
2018-11-01 19:32:39 +0000

    change to ex2.hs

--- ex2.hs
+++ ex2.hs
@@ -1,9 +1,7 @@
-import Data.List
+--import Data.List (maximumBy)
 import Data.Function (on)
-data Set a = Empty | Set a (Set a)
-  deriving (Read, Show)
-
-
+import Data.Ord (comparing)
+import Data.List
 lcs :: Ord a => [a] -> [a] -> [a]
 lcs xs ys = snd $ lcs' xs ys
 
@@ -17,15 +15,6 @@ lcs' (x:xs) (y:ys)
 lcs' [] _ = (0, [])
 lcs' _ [] = (0, [])
 
-fromList :: [a] -> Set a
-fromList []     = Empty
-fromList (x:xs) = Set x (fromList xs)
-
-a = [1,3,5,4,2,5,4,6,1,2]
-test = fromList a --WTF tho ?
-
-
-
 {--
 What is r1 and r2, l1 and l2 in this case ?
 r1 and r2 with @ is the result of the function that have first index is l1 or l2
@@ -35,27 +24,43 @@ good example, remember this
 Hence, to do ex2 using list comprehension and recursion to go compare every elt while saving the largest sub set
 --}
 
--- test = [[1,2,3], [0,1,3], [1,3,4]]
--- test2 = maximumBy (compare `on` length)  (helper test)
---
--- a = [1,3,5,4,2,5,4,6,1,2]
---
--- b = [6,2,3,4]
---
--- c = [1,2,3]
+
+a = [1,3,5,4,2,5,4,6,1,2]
+
+b = [6,2,3,4]
+
+c = [1,2,3]
 
 
 -- Exercise 2
 -- longest common sub-list of a finite list of finite list
 
-longestCommonSubList :: Eq a => [[a]] -> [a]
-longestCommonSubList [[]] = []
--- longestCommonSubList xs =
---  where
---   list = sort $ concat xs
+--helper function
+helper :: Ord a => [[a]] -> [[a]]
+helper xs = [lcs lists1 lists2 | lists1 <- xs, lists2 <- xs, lists1/=lists2]
 
-getList :: Eq a => [[a]] -> [a]
-getList [[]] = []
-getList (x:xs) =
- where
-  list = sort $ concat (x:xs)
+lsort :: [[a]] -> [[a]]
+lsort = sortBy (comparing length)
+
+
+
+-- longestCommonSubList :: Eq a => [[a]] -> [a]
+-- longestCommonSubList [[]] = []
+-- longestCommonSubList xs =  maximumBy (compare `on` length)  (helper xs)
+
+test = [[1,2,3,4], [0,1,3,4], [1,3,4],[1],[1,2,3]]
+test2 = maximumBy (compare `on` length)  (helper test)
+test3 = reverse lsort (helper test)
+
+
+--TODO: need to change this method to higher order
+checkLCS :: [Int] -> [Int] -> Bool
+checkLCS l1 l2 = (lcs l1 l2) == l2
+
+helper2 :: [[Int]] -> [[Int]]
+helper2 common input = [ | x <- input, filter (checkLCS x) ]
+
+
+
+--TODO: write a function to sort the result of the helper Function
+-- using filter and lcs the check whether the list work if yes return the list else check the other ?
