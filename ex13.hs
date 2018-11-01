4ec1adbe9d0de87e9e229f1a2dfbbab38e3e67e3
Hieu T Luu <thl1g15@soton.ac.uk>
2018-11-01 14:45:38 +0000

    debug ex13.hs

--- ex13.hs
+++ ex13.hs
@@ -34,3 +34,57 @@ test = get 0 testElt
 index = elemIndex [1..] [[1..],[2..],[3..]]
 
 index2 = elemIndex 1 [1,2,3]
+
+emptyIntList :: [Int]
+emptyIntList = []
+
+emptyIntListList :: [[Int]]
+emptyIntListList = []
+
+-- a couple of streams to test
+testStream1 :: [[Int]]
+testStream1 = [0..]:testStream1
+testStream2 :: [[Int]]
+testStream2 = repeatStream 0
+repeatStream :: Int -> [[Int]]
+repeatStream x = [x..]:(repeatStream (x+1))
+
+-- test stream is actually different (looking at first n elements only)
+isDifferentStream :: [[Int]] -> [Int] -> Int -> Bool
+isDifferentStream nns ns n =
+    let ns' = take n ns
+        nns' = map (take n) (take n nns)
+    in length ns' == 10 &&
+       all (\xs -> length xs == 10) nns' &&
+       not (ns' `elem` nns')
+
+-- coarse test for approximation floating point equality
+approxEqual :: Float -> Float -> Bool
+approxEqual x y = abs (x - y) < 1e-2
+
+-- Main program checks the results of the tests and produces scores
+
+-- Auxiliary functions to support testing
+message :: [(String,Bool)] -> String
+message ts =
+  let failures = [(s,b) | (s,b) <- ts , b == False] in
+  if failures == [] then "All test cases passed"
+  else "Failed " ++ (intToString (length failures)) ++ " out of " ++ (intToString (length ts)) ++ " test cases"
+
+intToString :: Int -> String
+intToString 0 = "0"
+intToString n =
+  if n < 10 then digitToString n
+  else (intToString (n `div` 10)) ++ (digitToString (n `mod` 10))
+
+digitToString :: Int -> String
+digitToString 0 = "0"
+digitToString 1 = "1"
+digitToString 2 = "2"
+digitToString 3 = "3"
+digitToString 4 = "4"
+digitToString 5 = "5"
+digitToString 6 = "6"
+digitToString 7 = "7"
+digitToString 8 = "8"
+digitToString 9 = "9"
