1ad58e5b4f5e3fcaa9639572e4bd8e808805b813
Hieu T Luu <thl1g15@soton.ac.uk>
2018-11-04 19:14:32 +0000

    need to do some debug

--- ex11.hs
+++ ex11.hs
@@ -12,16 +12,16 @@ merge (x:xs) (y:ys) = x : y : merge xs ys
 
 
 createRectangles :: (Int, Int) -> [Rectangle]
-createRectangles (0,_) = []
-createRectangles (_,0) = []
+createRectangles (0,_) = [Rectangle (0,0) (0,0)]
+createRectangles (_,0) = [Rectangle (0,0) (0,0)]
 createRectangles (a,b) = Rectangle (-a,-b) (a,b): ( merge (createRectangles (a-1,b)) (createRectangles (a,b-1)) )
 
 finalRectangleList :: [Rectangle] -> [Rectangle]
 finalRectangleList = Set.toList . Set.fromList
 
 isInEclipse :: Float -> Float -> Float -> Float -> Rectangle -> Bool
-isInEclipse x y a b (Rectangle (mx,nx) (px,qx)) | (m-x)^2 `div` a^2 + (n-x)^2 `div` b^2 > 1 = False
-                                                | (p-x)^2 `div` a^2 + (q-x)^2 `div` b^2 > 1 = False
+isInEclipse x y a b (Rectangle (mx,nx) (px,qx)) | (m-x)^2 / a^2 + (n-x)^2 / b^2 > 1 = False
+                                                | (p-x)^2 / a^2 + (q-x)^2 / b^2 > 1 = False
                                                 | otherwise = True
  where
   m = fromIntegral mx
@@ -31,7 +31,9 @@ isInEclipse x y a b (Rectangle (mx,nx) (px,qx)) | (m-x)^2 `div` a^2 + (n-x)^2 `d
 --isInEclipse :: Rectangle -> Bool
 -- TODO : finish this method than using filter to reduce the number of rectangles then use the result of ex10 to finish this
 
--- drawEllipse :: Float -> Float -> Float -> Float -> [Rectangle]
--- drawEllipse x y a b = filter (isInEclipse x y a b) list
---  where
---   list = finalRectangleList $ createRectangles (a,b)
+drawEllipse :: Float -> Float -> Float -> Float -> [Rectangle]
+drawEllipse x y a b = filter (isInEclipse x y a b) list
+ where
+  ax = floor a
+  bx = floor b
+  list = finalRectangleList $ createRectangles (ax,bx)
