cf47a0d4d7caa20c1e3fbd18cfd914a050cb4d61
Hieu T Luu <thl1g15@soton.ac.uk>
2018-11-10 16:16:20 +0000

    done ex14

--- ex14.hs
+++ ex14.hs
@@ -1,9 +1,15 @@
 -- Exercise 14
 -- extract both components from a square shell pair and apply the (curried) function
 unPairAndApply :: Int -> (Int -> Int -> a) -> a
-unPairAndApply n f = f 0 0
+unPairAndApply n f = f x y
+ where
+  x = fst $ unPair n
+  y = snd $ unPair n
 
 unPair :: Int -> (Int,Int)
 unPair z | (z - m^2) < m = (z - m^2, m)
          | otherwise = (m , m^2 + 2*m -z)
- where m = floor $ fromIntegral $ sqrt z -- what do I have to do with you ?
+ where m = isqrt z
+
+isqrt :: Int -> Int
+isqrt = floor . sqrt . fromIntegral
